c6871d32c7eaa8405c257386bc7d0bab0f546f98
mishpro <tabatcikov2010@gmail.com>
2024-03-31 13:13:26 +0700

    BetterCode

--- src/main.cpp
+++ src/main.cpp
@@ -7,20 +7,18 @@ class $modify(MProfilePage, ProfilePage) {
 
 	void loadPageFromUserInfo(GJUserScore* p0) {
 		ProfilePage::loadPageFromUserInfo(p0);
-		//170.5,-10
-		auto sprite = as<CCNode*>(this->getChildren()->objectAtIndex(0))->getChildByID("player-menu")->getChildByID("player-icon");
-		auto btn = CCMenuItemSpriteExtra::create(sprite->getChildByID("player-icon"), this, menu_selector(MProfilePage::onIconsInfo));
-		sprite->removeFromParentAndCleanup(false);
-		btn->setPosition(sprite->getPositionX(), sprite->getPositionY());
-		btn->setContentSize(42.6f);
 		auto menu = as<CCNode*>(this->getChildren()->objectAtIndex(0))->getChildByID("player-menu");
-		if(menu){
-			menu->addChild(btn);
-			//menu->updateLayout();
-		}
+		
+		auto cSize = new CCSize(42.6f, 42.6f);
+		auto cubeSprite = menu->getChildByID("player-icon");
+		auto cubeBtn = CCMenuItemSpriteExtra::create(cubeSprite->getChildByID("player-icon"), this, menu_selector(MProfilePage::onCubeInfo));
+		cubeSprite->removeFromParentAndCleanup(false);
+		cubeBtn->setPosition(cubeSprite->getPositionX(), cubeSprite->getPositionY());
+		cubeBtn->setContentSize(cSize);
+		menu->addChild(btn);
 	}
 
-	void onIconsInfo(CCObject*) {
+	void onCubeInfo(CCObject*) {
 		auto popup = ItemInfoPopup::create(m_score->m_playerCube, GameManager::sharedState()->iconTypeToUnlockType(IconType::Cube));
 		popup->setZOrder(this->getZOrder() + 1);
 		CCScene::get()->addChild(popup);
